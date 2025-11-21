febd6330c5c1fda9c13c4ef0f83eb40940c569a3
HVPasindu <hvpmjayarathna2001@gmail.com>
2025-11-21 21:58:50 +0530

    Initial commit for DentPulse backend

---
+++ src/main/java/com/dentpulse/dentalsystem/DentpulseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dentpulse.dentalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentpulseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentpulseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dentpulse/dentalsystem/DentpulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentpulse.dentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentpulseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
