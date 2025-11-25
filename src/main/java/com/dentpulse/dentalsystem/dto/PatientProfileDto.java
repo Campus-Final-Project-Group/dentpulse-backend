b746ee25c2ea8af07200b7b65af1f61604dc947f
HVPasindu <hvpmjayarathna2001@gmail.com>
2025-11-25 11:55:53 +0530

    add DTOs for patient profile and update request

---
+++ src/main/java/com/dentpulse/dentalsystem/dto/PatientProfileDto.java
@@ -0,0 +1,12 @@
+package com.dentpulse.dentalsystem.dto;
+
+import lombok.Data;
+
+@Data
+public class PatientProfileDto {
+    private String fullName;
+    private String email;
+    private String phone;
+    private String birthDate;
+    private String address;
+}
