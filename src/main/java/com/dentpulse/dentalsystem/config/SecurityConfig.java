1c723178ec6d181c193d94c74b95200fe7625d54
sahanBasnayake <sahanbasnayake888@gmail.com>
2025-12-19 17:48:28 +0530

    give the permission for admin module.Just for developing purpose

--- src/main/java/com/dentpulse/dentalsystem/config/SecurityConfig.java
+++ src/main/java/com/dentpulse/dentalsystem/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
-                        .requestMatchers("/dentpulse/api/v1/admin/create").permitAll()
+                        .requestMatchers("/dentpulse/api/v1/admin/**").permitAll()
                         //.requestMatchers("/dentpulse/api/v1/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
