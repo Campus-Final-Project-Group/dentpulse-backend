3e07e1940b638d734fde7a362f0e520725679977
Vishara Srinadi <visharasrinadi@gmail.com>
2025-12-23 13:55:13 +0530

    Updated AppointmentDTO with new variable names.

--- src/main/java/com/dentpulse/dentalsystem/dto/AppointmentDTO.java
+++ src/main/java/com/dentpulse/dentalsystem/dto/AppointmentDTO.java
@@ -1,19 +1,23 @@
 package com.dentpulse.dentalsystem.dto;
 
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AppointmentDTO {
-    private int appointment_id;
-    private String patient_name;
-    private Time start_time;
-    private Date appointment_date;
+    private Long appointmentId;
+    private String fullName;
+    private LocalDate appointmentDate;
+
+    private LocalTime startTime;
     private Enum status;
 }
