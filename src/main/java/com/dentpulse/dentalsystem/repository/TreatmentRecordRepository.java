345a883e1c158eefa7fcf9eb40b985a1fcf5edb1
Vishara Srinadi <visharasrinadi@gmail.com>
2026-01-05 00:49:05 +0530

    Updated TreatmentRecordRepository.java file

--- src/main/java/com/dentpulse/dentalsystem/repository/TreatmentRecordRepository.java
+++ src/main/java/com/dentpulse/dentalsystem/repository/TreatmentRecordRepository.java
@@ -3,5 +3,10 @@ package com.dentpulse.dentalsystem.repository;
 import com.dentpulse.dentalsystem.entity.TreatmentRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TreatmentRecordRepository extends JpaRepository<TreatmentRecord, Long> {
+    List<TreatmentRecord> findByPatientId(Long patientId);
+
 }
+
