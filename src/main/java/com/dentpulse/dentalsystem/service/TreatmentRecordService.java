8488be3adf05a371e2c45294bff3e7465a458856
Vishara Srinadi <visharasrinadi@gmail.com>
2026-01-05 00:50:08 +0530

    Updated TreatmentRecordService.java file

--- src/main/java/com/dentpulse/dentalsystem/service/TreatmentRecordService.java
+++ src/main/java/com/dentpulse/dentalsystem/service/TreatmentRecordService.java
@@ -1,12 +1,17 @@
 package com.dentpulse.dentalsystem.service;
 
+import com.dentpulse.dentalsystem.dto.PatientListDto;
 import com.dentpulse.dentalsystem.dto.TreatmentRecordDTO;
+import com.dentpulse.dentalsystem.entity.Patient;
 import com.dentpulse.dentalsystem.entity.TreatmentRecord;
+import com.dentpulse.dentalsystem.repository.PatientRepository;
 import com.dentpulse.dentalsystem.repository.TreatmentRecordRepository;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Transactional
@@ -17,9 +22,22 @@ public class TreatmentRecordService {
     private ModelMapper modelMapper;
     @Autowired
     private TreatmentRecordRepository treatmentRecordRepo;
+
+    @Autowired
+    private PatientRepository patientRepositoryRepo;
     public TreatmentRecordDTO updateRecords(TreatmentRecordDTO treatmentrecordDTO){
         treatmentRecordRepo.save(modelMapper.map(treatmentrecordDTO, TreatmentRecord.class));
         return treatmentrecordDTO;
 
     }
+
+
+    public PatientListDto getRecordsByPatientId(Long patientId) {
+        return patientRepositoryRepo.findById(patientId)
+                .map(patient -> modelMapper.map(patient, PatientListDto.class))
+                .orElseThrow(() -> new RuntimeException("Patient not found with id: " + patientId));
+    }
+
+
+
 }
