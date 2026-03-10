8dd1c271ef6d9021eede3b880f1e80cc2da525f8
kalanaashen <northernwolf1472@gmail.com>
2026-03-10 13:17:35 +0530

    add pagination for nofitication

--- src/main/java/com/dentpulse/dentalsystem/controller/ReminderController.java
+++ src/main/java/com/dentpulse/dentalsystem/controller/ReminderController.java
@@ -5,6 +5,10 @@ import com.dentpulse.dentalsystem.service.ReminderService;
 import com.dentpulse.dentalsystem.repository.ReminderLogRepository;
 import com.dentpulse.dentalsystem.entity.ReminderLog;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
 
 import java.util.List;
 
@@ -22,7 +26,12 @@ public class ReminderController {
     }
 
     @GetMapping("/logs")
-    public List<ReminderLog> getReminderLogs() {
-        return reminderLogRepository.findAllByOrderBySentAtDesc();
+    public List<ReminderLog> getReminderLogs(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size
+
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        return reminderLogRepository.findAllByOrderBySentAtDesc(pageable);
     }
 }
--- src/main/java/com/dentpulse/dentalsystem/repository/ReminderLogRepository.java
+++ src/main/java/com/dentpulse/dentalsystem/repository/ReminderLogRepository.java
@@ -2,11 +2,14 @@ package com.dentpulse.dentalsystem.repository;
 
 import com.dentpulse.dentalsystem.entity.ReminderLog;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.dentpulse.dentalsystem.entity.ReminderLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface ReminderLogRepository extends JpaRepository<ReminderLog, Long> {
 
-    List<ReminderLog> findAllByOrderBySentAtDesc();
+    List<ReminderLog> findAllByOrderBySentAtDesc(Pageable pageable);
 
 }
