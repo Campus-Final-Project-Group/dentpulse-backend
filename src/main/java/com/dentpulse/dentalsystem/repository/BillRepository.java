47228b05f19d812d20a4c4338ba428e2169742e3
sahanBasnayake <sahanbasnayake888@gmail.com>
2026-03-10 11:52:13 +0530

    update(admin): update patient history details

--- src/main/java/com/dentpulse/dentalsystem/repository/BillRepository.java
+++ src/main/java/com/dentpulse/dentalsystem/repository/BillRepository.java
@@ -1,12 +1,23 @@
 package com.dentpulse.dentalsystem.repository;
 
 import com.dentpulse.dentalsystem.entity.Bill;
+import com.dentpulse.dentalsystem.entity.TreatmentType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface BillRepository extends JpaRepository<Bill, Long> {
 
     List<Bill> findByBillDate(LocalDate billDate);
+
+    Optional<Bill> findByPatientIdAndTreatmentService_TreatmentType(
+            Long patientId,
+            TreatmentType treatmentType
+    );
+
+    List<Bill> findByPatientId(Long patientId);
+
+    Optional<Bill> findByPatientIdAndBillDate(Long patientId, LocalDate billDate);
 }
