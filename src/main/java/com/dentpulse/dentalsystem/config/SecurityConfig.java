5bff12fef11e0e72f9e5520748fd322976bca4a1
HVPasindu <hvpmjayarathna2001@gmail.com>
2026-03-11 19:42:38 +0530

    update security

--- src/main/java/com/dentpulse/dentalsystem/config/SecurityConfig.java
+++ src/main/java/com/dentpulse/dentalsystem/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
                 "http://localhost:5173",
                 "https://www.dentpulseclinic.com",
                 "https://dentpulseclinic.com"
+                "https://api.dentpulseclinic.com"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
